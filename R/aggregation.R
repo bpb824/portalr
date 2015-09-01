e03e622aa10df0b18c1f682ec0ddd462b1da9d35
bpb824 <bryanpblanc@gmail.com>
2015-09-01 16:50:38 -0700

    fixed up

--- R/aggregation.R
+++ R/aggregation.R
@@ -1,7 +1,13 @@
-require(plyr)
-require(lubridate)
-
-aggTime= function(unAgg, aggVars, timeCut, hod = TRUE){
+#' Aggregate PORTAL data over time.
+#'
+#' @param unAgg Data that you want to aggregate.
+#' @param aggVars Variables you want to retain in aggregation, such as 'lanenumber' or 'stationid'
+#' @param timeCut A string indicating how you want to aggregate over time. Examples include '1 min', '5 min', '1 hour', '1 day', etc.
+#' @param hod A boolean indicating weather you want to aggregate by hour of day across multiple days.
+#'
+#' @return Aggregated data.
+#' @export
+aggTime= function(unAgg, aggVars, timeCut, hod = FALSE){
   unAgg$period = cut(unAgg$starttime,breaks =timeCut)
   agg = ddply(unAgg, c(aggVars,"period"),function(X) data.frame(volume=sum(X$volume), occupancy = mean(X$occupancy), speed = weighted.mean(X$speed,X$volume)))
   agg$period = as.POSIXct(agg$period)
